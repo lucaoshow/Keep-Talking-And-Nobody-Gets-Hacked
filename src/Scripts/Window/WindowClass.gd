c605d42a16801eeb199a956d8b6b0c618408ab82
lucaoshow <lucas.nunes@sou.inteli.edu.br>
2023-12-14 12:06:05 -0300

    refactor + upgraded typing logic
    needs more refactoring but thats all i can do for now... also the logic is incomplete, the text gets out of the window if you type too much, but ill fix it

--- src/Scripts/Window/WindowClass.gd
+++ src/Scripts/Window/WindowClass.gd
@@ -1,13 +1,14 @@
 extends Node2D
 class_name WindowDisplay
 
+const _TERMINAL_PATH : String = "C:\\WINDOWS\\System32\\"
+
 enum States {IDLE = 1, RESIZING, MOVING}
 var _currentState : States = States.IDLE
 
 var _windowSprite : Sprite2D = Sprite2D.new()
 var _typingSpace : LineEdit = LineEdit.new()
-
-const _terminalPath : String = "C:\\WINDOWS\\System32\\"
+var _windowText : Label = Label.new()
 
 var _targetScaleX : float
 var _targetScaleY : float
@@ -15,30 +16,36 @@ var _t : float
 var _targetPosition : Vector2
 var _elapsedTime : float
 
+# CONSTRUCTOR
 
-# change the standard sprite later
-func _init(widthScale : float, heightScale : float, typeable : bool = true, windowTexture : CompressedTexture2D = preload("res://Assets/Terminal/terminal.png")):
+func _init(widthScale : float, heightScale : float, typeable : bool = true,
+			windowTexture : CompressedTexture2D = preload("res://Assets/Terminal/terminal.png"),
+			font : FontFile = preload("res://Assets/Terminal/CMD.ttf"), fontSize : int = 18):
+
 	_windowSprite.scale.x = widthScale
 	_windowSprite.scale.y = heightScale
 	_windowSprite.texture = windowTexture
 	add_child(_windowSprite)
+
+	var reference : Rect2 = _windowSprite.get_rect()
+	
+	WindowUtils.configureWindowTextObj(_windowText, font, fontSize, reference)
+	_windowSprite.add_child(_windowText)
+	
 	if typeable:
-		var font : FontFile = preload("res://Assets/Terminal/CMD.ttf")
-		var reference : Rect2 = _windowSprite.get_rect()
-		_typingSpace.scale = Vector2(1.5,1.5)
-		_typingSpace.size = Vector2(reference.size.x - 40, 40) # magical numbers
-		_typingSpace.position = reference.position + Vector2(10, 40) # magical numbers
-		_typingSpace.flat = true
-		_typingSpace.set("theme_override_styles/focus", StyleBoxEmpty.new())
-		_typingSpace.set("theme_override_fonts/font", font)
-		_typingSpace.set("theme_override_font_sizes/font_size", 18)
-		_typingSpace.text = _terminalPath
+		WindowUtils.configureWindowTextObj(_typingSpace, font, fontSize, reference)
+		_typingSpace.text = _TERMINAL_PATH
+		
 		_typingSpace.connect("text_changed", _blockterminalPathErasing)
 		_typingSpace.connect("text_submitted", _enterCommand)
+		
 		_windowSprite.add_child(_typingSpace)
+
 	else:
 		_typingSpace.free()
 
+
+# PUBLIC METHODS
 
 func resize(widthPercentage : float, heightPercentage : float, t : float):
 	_targetScaleX = _windowSprite.scale.x * widthPercentage
@@ -49,8 +56,11 @@ func resize(widthPercentage : float, heightPercentage : float, t : float):
 
 func moveTo(pos : Vector2, t : float):
 	_targetPosition = pos
+	_t = t
 	_currentState = States.MOVING
 
+
+# PRIVATE METHODS
 
 func _checkActionFinished():
 	if _elapsedTime >= _t:
@@ -59,18 +69,26 @@ func _checkActionFinished():
 
 
 func _scale(delta : float):
-	var tOverTime = _t * delta
-	_elapsedTime += tOverTime
-	_windowSprite.scale.x = lerpf(_windowSprite.scale.x, _targetScaleX, tOverTime)
-	_windowSprite.scale.y = lerpf(_windowSprite.scale.y, _targetScaleY, tOverTime)
+	var tPerFrame = _t * delta
+	_elapsedTime += tPerFrame
+	_windowSprite.scale.x = lerpf(_windowSprite.scale.x, _targetScaleX, tPerFrame)
+	_windowSprite.scale.y = lerpf(_windowSprite.scale.y, _targetScaleY, tPerFrame)
 	_checkActionFinished()
 
 
 func _reposition(delta : float):
-	_elapsedTime += delta * _t
-	position = position.lerp(_targetPosition, _t * delta)
+	var tPerFrame = _t * delta
+	_elapsedTime += tPerFrame
+	position = position.lerp(_targetPosition, tPerFrame)
 	_checkActionFinished()
 
+
+func _resetTerminalText():
+	_typingSpace.text = _TERMINAL_PATH
+	_typingSpace.caret_column = len(_TERMINAL_PATH)
+
+
+# UPDATE PER FRAME
 
 func _process(delta):
 	if _currentState == States.RESIZING:
@@ -80,11 +98,15 @@ func _process(delta):
 		_reposition(delta)
 
 
+# CALLABLES FOR SIGNALS
+
 func _blockterminalPathErasing(newText : String):
-	if len(_typingSpace.text) <= len(_terminalPath):
-		_typingSpace.text = _terminalPath
-		_typingSpace.caret_column = len(_terminalPath)
+	if len(_typingSpace.text) <= len(_TERMINAL_PATH):
+		_resetTerminalText()
 
 
 func _enterCommand(newText : String):
-	_typingSpace.text += "\n" + _terminalPath
+	_windowText.text += newText + "\n"
+	_typingSpace.clear()
+	_typingSpace.position.y += _typingSpace.size.y
+	_resetTerminalText()
---
+++ src/Scripts/Window/WindowUtils.gd
@@ -0,0 +1,21 @@
+class_name WindowUtils
+
+const _PIXELATE_TEXT_SCALE : Vector2 = Vector2(1.5, 1.5)
+const _SIZE_OFFSET : int = 40
+const _WINDOW_TEXT_POS_OFFSET : Vector2 = Vector2(16, 56)
+const _TYPING_SPACE_POS_OFFSET : Vector2 = Vector2(10, 40)
+
+static func configureWindowTextObj(obj : Object, font : FontFile, fontSize : int, reference : Rect2):
+	obj.scale = _PIXELATE_TEXT_SCALE
+	obj.size = Vector2(reference.size.x - _SIZE_OFFSET, _SIZE_OFFSET)
+	
+	if obj.get_class() == "LineEdit":
+		obj.position = reference.position + _TYPING_SPACE_POS_OFFSET
+		obj.flat = true
+		obj.set("theme_override_styles/focus", StyleBoxEmpty.new())
+	
+	else:
+		obj.position = reference.position + _WINDOW_TEXT_POS_OFFSET
+	
+	obj.set("theme_override_fonts/font", font)
+	obj.set("theme_override_font_sizes/font_size", fontSize)
