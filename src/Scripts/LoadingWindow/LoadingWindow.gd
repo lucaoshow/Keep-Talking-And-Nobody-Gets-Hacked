0085ec35fb011eb86d3ff71d41fd525e1d5f1bfb
lucaoshow <lucas.nunes@sou.inteli.edu.br>
2024-01-31 00:06:33 -0300

    add loading bar window logic
    added the base logic for the loading bar window (displaying time and lose function template)

---
+++ src/Scripts/LoadingWindow/LoadingWindow.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+@onready var time : Label = $RemainingTime
+@onready var counter : Timer = $Timer
+
+const Y_POS : int = 578
+var goingUp : bool = false
+var seconds : int = 59
+var minutes : int = 6
+
+func startLoading():
+	counter.start()
+	goingUp = true
+
+
+func goUp():
+	if (position.y <= Y_POS):
+		goingUp = false
+		return
+
+	position -= Vector2(0, 2)
+
+
+func updateTime():
+	seconds -= counter.wait_time
+	if (seconds < 0 and minutes > 0):
+		seconds = 59
+		minutes -= 1
+	time.text = "Remaining time: " + str("%02d" % minutes) + ":" + str("%02d" % seconds)
+
+
+func updateProgressBar():
+	$LoadingBar.value += 1
+	if ($LoadingBar.value == $LoadingBar.max_value):
+		counter.stop()
+		lose()
+
+
+func lose():
+	pass
+
+
+func _process(delta):
+	if (goingUp):
+		goUp()
+
+
+func _on_timer_timeout():
+	updateTime()
+	updateProgressBar()
